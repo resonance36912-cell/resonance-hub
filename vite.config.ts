309c37b64d2d324c09b42865d323163d5d32a2e3
ResonanceAppDev <ashleyuys@medi-tech.life>
2026-09-22 06:13:21 +0200

    style: format changed Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -24,14 +24,23 @@ export default defineConfig({
     ],
   },
   optimizeDeps: {
-    include: ["react", "react-dom", "react-dom/client", "react/jsx-runtime", "react/jsx-dev-runtime"],
+    include: [
+      "react",
+      "react-dom",
+      "react-dom/client",
+      "react/jsx-runtime",
+      "react/jsx-dev-runtime",
+    ],
     ignoreOutdatedRequests: true,
   },
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes("/node_modules/@supabase/") || id.includes("\\node_modules\\@supabase\\")) {
+          if (
+            id.includes("/node_modules/@supabase/") ||
+            id.includes("\\node_modules\\@supabase\\")
+          ) {
             return "vendor-supabase";
           }
         },
