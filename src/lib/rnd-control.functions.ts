b9863940d5a6e1d08430cb4615b2616cad5d77a9
ResonanceAppDev <ashleyuys@medi-tech.life>
2026-09-23 22:48:18 +0200

    chore(rnd): document dynamic boundary typing

--- src/lib/rnd-control.functions.ts
+++ src/lib/rnd-control.functions.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { createServerFn } from "@tanstack/react-start";
 import { getRequest } from "@tanstack/react-start/server";
 import { z } from "zod";
